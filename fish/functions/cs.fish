3707d2c3d523c678c11390d084a869ffd560ebdc
David Chen <chenwei2000121@163.com>
2019-08-28 15:10:45 +0800

    compton, llpp, ranger and other stuff

---
+++ fish/functions/cs.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function cs --description 'alias cs cowsay'
+	cowsay  $argv;
+end
---
+++ fish/functions/ipy.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function ipy --description 'alias ipy ipython'
+	ipython  $argv;
+end

e02135b4ddfac1fd903ac16319174c1310d4642f
David Chen <chenwei2000121@163.com>
2019-09-14 16:02:04 -0700

    fzf, onedrive

--- fish/functions/fzf.fish
+++ fish/functions/fzf.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function fzf --description alias\ fzf\ fzf\ --preview\ \'head\ -100\ \{\}\'
-	command fzf --preview 'head -100 {}' $argv;
+function fzf --description alias\ fzf\ fzf\ --preview\ \'head\ -100\ \{\}\'\n
+	command fzf --preview 'head -100 {}'  $argv;
 end
---
+++ fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/david/.fzf/shell/key-bindings.fish

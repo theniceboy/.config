a86968674524807b7cb840da96702d07afead249
David Chen <chenwei2000121@163.com>
2019-10-07 17:49:54 -0700

    new stuff

---
+++ fish/functions/ms.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function ms --description 'alias ms mailsync'
+	mailsync  $argv;
+end
---
+++ fish/functions/mt.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function mt --description 'alias mt neomutt'
+	neomutt  $argv;
+end

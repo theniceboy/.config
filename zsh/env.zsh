9bf13cae5ee06b1ddace1ac9bcaabe8d1730af16
David Chen <chenwei2000121@163.com>
2020-04-15 17:42:23 -0400

    env var updates

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,17 +1,18 @@
+export XDG_CONFIG_HOME=$HOME/.config
+export LOCALBIN=$XDG_CONFIG_HOME/bin
+export PATH=$PATH:$LOCALBIN
+export LOCALPROG=$HOME/prog
 export GOPATH=$HOME/go
 export PATH=$PATH:$HOME/.gem/ruby/2.6.0
 export PATH=$PATH:/usr/local/bin
 export PATH=$PATH:$HOME/go/bin
 export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
 export PATH=$PATH:$HOME/.local/bin
-export PATH=$PATH:$HOME/.config/bin
 export PATH=$PATH:/snap/bin
 #export TERM=xterm-256color
-export PATH=$PATH:/home/david/prog/flutter/bin
-export PATH=$PATH:/home/david/prog/flutter/bin/cache/dart-sdk/bin
-export PATH=$PATH:/Users/david/prog/flutter/bin
+export PATH=$PATH:$LOCALPROG/flutter/bin
+export PATH=$PATH:$LOCALPROG/flutter/bin/cache/dart-sdk/bin
 export PATH="$PATH":"$HOME/.pub-cache/bin"
-export PATH=$PATH:/Users/david/prog/flutter/bin/cache/dart-sdk/bin
 export TERM=xterm-256color
 export TERM_ITALICS=true
 export RANGER_LOAD_DEFAULT_RC="false"
@@ -19,12 +20,4 @@ export RANGER_LOAD_DEFAULT_RC="false"
 export EDITOR=nvim
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
-export XDG_CONFIG_HOME=$HOME/.config
 
-# fzf
-export FZF_DEFAULT_OPTS='--bind ctrl-e:down,ctrl-u:up --preview "[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500"'
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-export FZF_COMPLETION_TRIGGER='\'
-export FZF_TMUX=1
-export FZF_TMUX_HEIGHT='80%'
-export fzf_preview_cmd='[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500'
--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,4 +1,11 @@
 export FZF_DEFAULT_OPTS='--bind=ctrl-t:top,change:top --bind ctrl-e:down,ctrl-u:up'
+#export FZF_DEFAULT_OPTS='--bind ctrl-e:down,ctrl-u:up --preview "[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500"'
+#export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+export FZF_DEFAULT_COMMAND='fd'
+export FZF_COMPLETION_TRIGGER='\'
+export FZF_TMUX=1
+export FZF_TMUX_HEIGHT='80%'
+export fzf_preview_cmd='[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500'
 
 
 _fzf_fpath=${0:h}/fzf

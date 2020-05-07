3f59cc2f6447e0ca250e3bfb351b9858f5e56f8b
David Chen <weichen2000121@gmail.com>
2020-05-07 13:53:04 -0400

    status updates

--- tmux/tmux-powerline/themes/default.sh
+++ tmux/tmux-powerline/themes/default.sh
@@ -38,7 +38,7 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		"ifstat 30 255" \
 		"ifstat_sys 30 255" \
 		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
+		#"wan_ip 24 255" \
 		#"vcs_branch 29 88" \
 		#"vcs_compare 60 255" \
 		#"vcs_staged 64 255" \
@@ -57,7 +57,7 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"cpu 240 136" \
 		#"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
+		#"battery 137 127" \
 		"weather 37 255" \
 		"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
 		#"xkb_layout 125 117" \

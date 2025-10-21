a753d243f358f76634ee68a7e834268ae99902d8
David Chen <darkwei233@gmail.com>
2025-10-20 23:23:35 -0700

    AGENTS.md updates

--- zsh/functions/co.zsh
+++ zsh/functions/co.zsh
@@ -1,6 +1,6 @@
 co() {
   local -a codex_cmd
-  codex_cmd=(codex --sandbox danger-full-access -m gpt-5 -c 'model_reasoning_summary_format=experimental' -c 'model_reasoning_effort=high' --search)
+  codex_cmd=(codex --sandbox danger-full-access -m gpt-5 -c 'model_reasoning_summary_format=experimental' -c 'model_reasoning_effort=high')
   local search_dir=$PWD
   local overlay_file=""
   while :; do
